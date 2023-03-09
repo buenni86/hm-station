9e61be852e3c7d879a57ee29c1cf0220a96d6f80
Nancy Meike <nancy.meike@deutschebahn.com>
2023-03-09 19:16:32 +0100

    test flyer

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
                 result_importance: "./src/result_importance.html",
                 result_location: "./src/result_location.html",
                 worldmap: "./src/worldmap.html",
-                flyer1: "./public/NW100_Azubi_Flyer_105x195_NEU_digital-data.pdf",
+                flyer1: "./src/flyer/NW101_DualesStudium_Flyer_105x195_digital-data.pdf",
                 ...getMapsScripts("./maps"),
             },
         },
